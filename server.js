323a086ff51226856aaa99fcaa8c3d57c260c153
Etherless <pabloimrik17@gmail.com>
2017-11-12 19:41:42 +0100

    Basic setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const hostname = 'localhost';
+const port = 8080;
+
+const server = http.createServer((request, response) => {
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.end('<h1>Hello World </h1>');
+
+});
+
+server.listen(port, hostname, () => {
+
+    console.log(`Server running at http://${hostname}:${port}`)
+
+})

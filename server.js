d43e639ad8f35e6b8b8e280a75714dd27c5aa419
Etherless <pabloimrik17@gmail.com>
2017-11-19 16:58:53 +0100

    Mongoose set up

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// ENV VARIABLES
+require('dotenv').config();
 
-
+// APP DEPENDENCIES
 const bodyParser = require('body-parser');
 // Consume the API from different origin
 const cors = require('cors'); 
 const console = require('console');
+const mongoose = require('mongoose');
+const express = require('express');
 
+// DATA
 const contacts = require('./data');
 
-const express = require('express');
 const app = express();
 
 app.use(bodyParser.urlencoded({'extended': true}));
@@ -101,6 +105,10 @@ const hostname = 'localhost';
 const port = 3001;
 
 app.listen(port, hostname, () => {
+
+    mongoose.connect(process.env.MONGO_DB_URL, { useMongoClient: true });
+    mongoose.Promise = global.Promise;
+    console.log(mongoose.model('Contacts').find());
 
     console.log(`Server running at http://${hostname}:${port}`)
 
